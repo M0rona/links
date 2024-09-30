41b75b3bf43498e9eefcbf5064abcb12ce76b232
M0rona <gabrielmorona0229@gmail.com>
2024-09-30 11:28:24 -0300

    Creating the links component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -5,6 +5,7 @@ import { colors } from "@/styles/colors";
 import { Category } from "@/components/category";
 import { categories } from "@/utils/categories";
 import { Categories } from "@/components/categories";
+import { Link } from "@/components/link";
 
 export default function Index() {
   return (
@@ -18,6 +19,8 @@ export default function Index() {
       </View>
 
       <Categories />
+
+      <Link name="Teste" url="TEste" onDetails={() => console.log("Clicou!")} />
     </View>
   );
 }
